f5e003433e962568b35287268c3d357e6fe26d09
Viktor Yakovenko <yakovenko.viktor.box@gmail.com>
2020-12-03 14:03:22 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>beginner</title>
+</head>
+<body>
+<?php
+echo '<p>Hello, <span style="font-weight:bold;">NIX education</span></p>';
+?>
+</body>
+</html>
+
