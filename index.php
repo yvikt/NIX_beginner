7680925f61b265edea15e2e021a1a3a620b774e0
Viktor Yakovenko <yakovenko.viktor.box@gmail.com>
2020-12-04 15:22:40 +0200

    made simple site layout

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>beginner</title>
+  <meta charset="UTF-8">
+  <title>beginner</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php
-echo '<p>Hello, <span style="font-weight:bold;">NIX education</span></p>';
-?>
+  <header>
+    Beginner PHP. Level 1
+  </header>
+    <main>
+    <nav>
+      <ul>
+        <li>
+          <a class="active" href="#">Главная</a></li>
+        <li>
+        <li>
+          <a href="#">урок 1</a></li>
+        <li>
+          <a href="#">урок 2</a></li>
+        <li class="right">
+          <a href="https://github.com/yvikt/NIX_beginner/commit/0937a2afef41efcdf3146c14d375d55111bd48c0">commit</a>
+        </li>
+      </ul>
+    </nav>
+      <section>
+        <?php echo 'content'; ?>
+      </section>
+    </main>
+  <footer>
+    <span>© 2020 Viktor Yakovenko</span>
+    | <a href="https://education.nixsolutions.com">NIX Education</a>
+    | <a href="https://github.com/yvikt/NIX_beginner">Github repository</a>
+  </footer>
 </body>
 </html>
-
